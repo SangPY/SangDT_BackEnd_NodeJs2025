2ad27b6b20b23c8e6cf694ad378a00c189a263a6
SangDoThanh <sangdothanh95@gmail.com>
2025-06-09 22:53:17 +0700

    Use Express Framework

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-// const { hostname } = require("os");
+const express = require('express')
+// import express from 'express';
 
-//LoadHTTPmodule(thưviệnđãcósẵnkhi cài đặtnode.js) 
-const http = require("http"); 
-const hostname = "127.0.0.1";//cái này===http://localhost 
-const port = 3000; 
+const app = express()
+const port = 8080
 
-//CreateHTTPserverand listenonport3000forrequests 
-const server=http.createServer((req,res)=>{ 
-//SettheresponseHTTPheaderwithHTTPstatusandContenttype 
-    res.statusCode=200; 
-    res.setHeader("Content-Type","text/plain"); 
-    res.end("Hello World\n Thanh Sang"); 
-}); 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-//listenforrequestonport3000,andasacallbackfunctionhavetheport listenedon logged server.listen(port,hostname,()=>{ 
-// console.log(`Serverrunningathttp://${hostname}:${port}/`); 
-// }); 
-//tựđộngchạytrên localhost 
-server.listen(port, hostname, ()=>{ 
-    console.log(`Server running at http://${hostname}:${port}/`); 
-}); 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
